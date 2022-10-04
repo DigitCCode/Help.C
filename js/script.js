d68734f00074dd7f71408d58bdb42d45f3d00a4c
Igor-Yngvi <96557071+Igor-Yngvi@users.noreply.github.com>
2022-10-04 08:45:13 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,19 +3,19 @@
 //Capturando o elemento de menu
 var navLinks = document.getElementById("navLinks");
 
-navLinks.style.display = "none";
+//navLinks.style.display = "none";
 
 //Função para mostrar o menu
 function showMenu(){
 	navLinks.style.right = "0";
-	navLinks.style.display = "block";
+	//navLinks.style.display = "block";
 }
 
 
 //Função para esconder o menu
 function hideMenu(){
         navLinks.style.right = "-200px";
-	navLinks.style.display = "none";
+	//navLinks.style.display = "none";
 }
 
 //Função que chama o popup
