ec22ad9d092d399b8a74aff35513179fe81c6b07
v1tooR <victorssantos572@gmail.com>
2022-12-06 17:04:56 -0300

    instuicoes 2

--- js/instituicoes.js
+++ js/instituicoes.js
@@ -3,14 +3,16 @@ const popup_container = document.getElementById('popup_container');
 const closing = document.getElementById('closing');
 const cad = document.getElementById('cadastro')
 
-opening.addEventListener('click', () => {
-    popup_container.classList.add('show');
-});
+function open(){
+        opening.addEventListener('click', () => {
+        popup_container.classList.add('show');
+    });
 
-closing.addEventListener('click', () => {
-    popup_container.classList.remove('show');
-});
+    closing.addEventListener('click', () => {
+        popup_container.classList.remove('show');
+    });
 
-cad.addEventListener('click', () => {
-    popup_container.classList.remove('show');
-});
+    cad.addEventListener('click', () => {
+        popup_container.classList.remove('show');
+    });
+}
